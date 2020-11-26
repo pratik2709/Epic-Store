d7043bc96b69007480c3f01c37eac3587e22245d
pratiksaxena <pratik2709@gmail.com>
2020-11-26 16:08:07 +0530

    intermediate for loading data

--- recommendations/management/commands/load_data.py
+++ recommendations/management/commands/load_data.py
@@ -9,16 +9,17 @@ class Command(BaseCommand):
     help = 'loads data in the database'
 
     def handle(self, *args, **options):
-        management.call_command('flush')
+        management.call_command('flush', interactive=False)
         df = pd.read_excel("data_tables.xlsx")
         print(df.iloc[0])
         print(df.to_dict())
         print(df.iloc[0]['Game Name'])
         for e in df.T.to_dict().values():
+            age = int(''.join(filter(str.isdigit, e['Target Age Group'])))
             game = Games.objects.create(name=e['Game Name'],
-                                 cover_url=e['Cover URL'],
-                                 age_group=e['Target Age Group'],
-                                 theme=e['Game Category: Theme'],
-                                 genre=e['Game Category: Genre'],
-                                 violence=e['Game Category: Violence'])
+                                        cover_url=e['Cover URL'],
+                                        age_group=age,
+                                        theme=e['Game Category: Theme'],
+                                        genre=e['Game Category: Genre'],
+                                        violence=e['Game Category: Violence'])
             print(game)
