43413eaeb8b178444935f839a203c870ef7d7e63
pratiksaxena <pratik2709@gmail.com>
2020-11-27 02:12:50 +0530

    intermediate for views

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,4 +1,7 @@
+import itertools
+
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
@@ -31,8 +34,19 @@ class RecommendationList(APIView):
 
     def get(self, request):
         # get the profile
-        # get the games according to the users according to age and then filter by genere, theme and violence
+        # get the games according to the users according to age
+        # and then filter by genere, theme and violence
         user_object = request.user
         profile = Profile.objects.get(user=user_object)
-        games = Games.objects.get()
-        return Response(profile.age)
+        profile_preferences = profile.preferences
+        profile_violence = profile_preferences.get('violence')
+        profile_genres = profile_preferences.get('genre')
+        profile_themes = profile_preferences.get('theme')
+        games_by_age_and_violence = Games.objects.filter(age_group__lte=profile.age, violence__in=profile_violence)
+        combinations = list(itertools.product(profile_themes, profile_genres))
+        query = Q()
+        for theme, genre in combinations:
+            query = query | Q(theme=theme, genre=genre)
+        res = games_by_age_and_violence.filter(query)
+        print(res)
+        return Response(res)
