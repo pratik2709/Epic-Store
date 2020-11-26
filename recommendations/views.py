a9869b97575c18b5fdb989a394e079e2fe6b2345
pratiksaxena <pratik2709@gmail.com>
2020-11-26 20:23:13 +0530

    intermediate for creating an API

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+# finds which user is logged in
+# write unoptimized query which returns data abased on age and attributes
+
+# create a login API for a user
+from rest_framework import viewsets, permissions
+
+from recommendations.models import Profile
+from recommendations.serializers import ProfileSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
