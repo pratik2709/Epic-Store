56e5cdd55b63a08d6a094cdcb02e5db165de988d
pratiksaxena <pratik2709@gmail.com>
2020-11-27 13:40:39 +0530

    intermediate

--- recommendations/tests.py
+++ recommendations/tests.py
@@ -9,6 +9,7 @@ from django.core import management
 
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
 from recommendations.models import Games, Attributes, Profile
+from recommendations.utils import Utility
 from recommendations.views import RecommendationList
 
 
@@ -23,10 +24,10 @@ class CreateNewPuppyTest(APITestCase):
         game_sheet = pd.read_excel(xls, 'Games')
         user_sheet = pd.read_excel(xls, 'Users')
         attribute_sheet = pd.read_excel(xls, 'Attributes')
-
-        add_games(game_sheet)
-        add_attributes(attribute_sheet)
-        add_users(user_sheet)
+        util = Utility()
+        util.add_games(game_sheet)
+        util.add_attributes(attribute_sheet)
+        util.add_users(user_sheet)
 
     def test_recommendations(self):
         factory = APIRequestFactory()
@@ -39,60 +40,3 @@ class CreateNewPuppyTest(APITestCase):
             print(user.first_name, json.loads(response.content))
 
 
-def add_games(game_sheet):
-    Games.objects.all().delete()
-    for e in game_sheet.T.to_dict().values():
-        age = int(''.join(filter(str.isdigit, e['Target Age Group'])))
-        game = Games.objects.create(name=e['Game Name'],
-                                    cover_url=e['Cover URL'],
-                                    age_group=age,
-                                    theme=e['Game Category: Theme'],
-                                    genre=e['Game Category: Genre'],
-                                    violence=e['Game Category: Violence'])
-        print(game)
-
-
-def add_attributes(attr_sheet):
-    Attributes.objects.all().delete()
-    print(attr_sheet.to_dict())
-    result = defaultdict(list)
-    attr_sheet = attr_sheet.replace({numpy.NaN: None})
-    for key, values in attr_sheet.to_dict().items():
-        for k, v in values.items():
-            result[key].append(v)
-
-    print(result)
-    Attributes.objects.create(attributes=result)
-
-
-def add_users(user_sheet):
-    Profile.objects.all().delete()
-    User.objects.all().exclude(username="admin").delete()
-    print(user_sheet.T.to_dict())
-
-    for e in user_sheet.T.to_dict().values():
-        result = dict()
-        result = {}
-        val = "Doesn't Matter"
-        val = val.lower()
-        result['theme'] = [x.strip() for x in e['Game Category: Theme'].split(',')]
-        result['theme'] = [value for value in result['theme'] if value.lower() != val]
-
-        # result['theme'] = ["Absent" if value == "None" else value for value in result['theme']]
-
-        result['genre'] = [x.strip() for x in e['Game Category: Genres'].split(',')]
-        result['genre'] = [value for value in result['genre'] if value.lower() != val]
-        # result['genre'] = ["Absent" if value == "None" else value for value in result['genre']]
-
-        result['violence'] = [x.strip() for x in e['Game Category: Violence'].split(',')]
-        result['violence'] = [value for value in result['violence'] if value.lower() != val]
-        # result['violence'] = ["Absent" if value == "None" else value for value in result['violence']]
-
-        print(result)
-        user = User.objects.create_user(username=e['Name'],
-                                        first_name=e['Name'],
-                                        email=e['Name'].lower() + '@estore.com',
-                                        password='password')
-        Profile.objects.create(user=user,
-                               age=e['Age'],
-                               preferences=result)
---
+++ recommendations/utils.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+
+from django.contrib.auth.models import User
+import pandas as pd
+
+from recommendations.models import Games, Attributes, Profile
+
+class Utility:
+
+    def add_games(self, game_sheet):
+        Games.objects.all().delete()
+        for e in game_sheet.T.to_dict().values():
+            age = int(''.join(filter(str.isdigit, e['Target Age Group'])))
+            violence = "Absent" if e['Game Category: Violence'] else e['Game Category: Violence']
+            Games.objects.create(name=e['Game Name'],
+                                 cover_url=e['Cover URL'],
+                                 age_group=age,
+                                 theme=e['Game Category: Theme'],
+                                 genre=e['Game Category: Genre'],
+                                 violence=violence)
+
+    def add_attributes(self, attr_sheet):
+        Attributes.objects.all().delete()
+        result = defaultdict(list)
+        attr_sheet.replace('None', 'Absent', inplace=True)
+        # print(attr_sheet.to_dict())
+        for key, values in attr_sheet.to_dict().items():
+            for k, v in values.items():
+                # print(v, type(v))
+                if not pd.isnull(v):
+                    result[key.lower()].append(v)
+        Attributes.objects.create(attributes=result)
+
+    def add_users(self, user_sheet):
+        Profile.objects.all().delete()
+        User.objects.all().exclude(username="admin").delete()
+        print(user_sheet.T.to_dict())
+
+        for e in user_sheet.T.to_dict().values():
+            attrs = Attributes.objects.all().first()
+            attrs = attrs.attributes
+            themes = attrs.get('themes')
+            genres = attrs.get('genres')
+            violence = attrs.get('violence')
+
+            result = {}
+            val = "Doesn't Matter"
+            result['theme'] = [x.strip() for x in e['Game Category: Theme'].split(',')]
+            result['theme'] = themes if result['theme'][0].lower() == val.lower() else result['theme']
+            result['theme'] = ["Absent" if value == "None" else value for value in result['theme']]
+
+            result['genre'] = [x.strip() for x in e['Game Category: Genres'].split(',')]
+            result['genre'] = genres if result['genre'][0].lower() == val.lower() else result['genre']
+            result['genre'] = ["Absent" if value == "None" else value for value in result['genre']]
+
+            result['violence'] = [x.strip() for x in e['Game Category: Violence'].split(',')]
+            result['violence'] = violence if result['violence'][0].lower() == val.lower() else result['violence']
+            result['violence'] = ["Absent" if value == "None" else value for value in result['violence']]
+
+            print(e['Name'],result)
+            user = User.objects.create_user(username=e['Name'],
+                                            first_name=e['Name'],
+                                            email=e['Name'].lower() + '@estore.com',
+                                            password='password')
+            Profile.objects.create(user=user,
+                                   age=e['Age'],
+                                   preferences=result)
